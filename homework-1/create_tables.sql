7c6fb94fdeb5ca5c891cc4c25b2f705bbc044b26
Дмитрий <dfeskov1998@mail.ru>
2023-11-11 13:59:51 +0300

    create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+-- SQL-команды для создания табли
+
+CREATE TABLE employees
+
+( 		employer_id int PRIMARY KEY,
+  		first_name varchar(20) not null,
+  		last_name varchar(20) not null,
+  		operation_id int UNIQUE
+
+);
+
+CREATE TABLE customers
+
+( 		customer_id int PRIMARY KEY,
+  		nickname varchar(20),
+  		operation_id int UNIQUE
+
+);
+
+CREATE TABLE orders
+
+( 		order_id int UNIQUE,
+  		operation_date date not null
+
+)
